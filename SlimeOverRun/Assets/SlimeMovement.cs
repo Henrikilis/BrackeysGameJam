a0fc98f168f802f83e7a7f6cbe75c872fd9bd32d
Henrikilis <73853874+Henrikilis@users.noreply.github.com>
2021-02-16 15:18:45 -0300

    movementInicial

---
+++ SlimeOverRun/Assets/SlimeMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float slimeSpeed;
+    public float slimeSuperSpeed;
+
+    public bool moveUp;
+    public bool accelerate;
+    public bool moveLeft;
+    public bool moveRight;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        moveUp = false;
+        accelerate = false;
+        moveLeft = false;
+        moveRight = false;
+    }
+
+    
+    void FixedUpdate()
+    {
+
+        if(moveUp)
+        rb.AddForce(new Vector3(slimeSpeed, 0), ForceMode.Acceleration);
+
+        if (accelerate)
+        rb.AddForce(new Vector3(slimeSuperSpeed, 0), ForceMode.Acceleration);
+
+        if (moveLeft)
+        rb.AddForce(new Vector3(0, 0, slimeSpeed), ForceMode.Acceleration);
+
+        if (moveRight)
+        rb.AddForce(new Vector3(0, 0, -slimeSpeed), ForceMode.Acceleration);
+
+    }
+
+}
---
+++ SlimeOverRun/Assets/UIController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+
+    public SlimeMovement sm;
+
+    private void Start()
+    {
+        sm = FindObjectOfType<SlimeMovement>();
+    }
+
+    public void left()
+    {
+        sm.moveLeft = true;
+        //
+        sm.moveRight = false;
+        sm.moveUp = false;
+        sm.accelerate = false;
+
+    }
+
+    public void right()
+    {
+        sm.moveRight = true;
+        //
+        sm.moveLeft = false;
+        sm.moveUp = false;
+        sm.accelerate = false;
+
+    }
+
+    public void halt()
+    {
+        sm.moveUp = true;
+        //
+        sm.moveLeft = false;
+        sm.moveRight = false;
+        sm.accelerate = false;
+
+    }
+
+    public void speed()
+    {
+        sm.accelerate = true;
+        //
+        sm.moveLeft = false;
+        sm.moveRight = false;
+        sm.moveUp = false;
+
+    }
+}
