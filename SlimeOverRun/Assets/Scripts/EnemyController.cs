7d3d2c7c37dc989d4ff3e88a21740cda3c44cc39
Gabriel Lo Monaco Honório <75794887+Gabriel-LF@users.noreply.github.com>
2021-02-17 16:05:41 -0300

    FullLevel

--- SlimeOverRun/Assets/Scripts/EnemyController.cs
+++ SlimeOverRun/Assets/Scripts/EnemyController.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int strengthNeeded;
+    public int currentStrength;
+    public bool attacking;
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            currentStrength++;
+            if (currentStrength >= strengthNeeded)
+            {
+                gameObject.SetActive(false);
+            }
+            else { StartCoroutine("Attack"); }
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            currentStrength--;
+        }
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") && attacking)
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        yield return new WaitForSeconds(1);
+        attacking = true;
+        yield return new WaitForSeconds(.4f);
+        attacking = false;
     }
 }
