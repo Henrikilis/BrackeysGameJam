f70ac825c459bf45fb24795822de040315aa46ef
Gabriel Lo Monaco Honório <75794887+Gabriel-LF@users.noreply.github.com>
2021-02-17 02:18:50 -0300

    Level Semi Completo

---
+++ SlimeOverRun/Assets/Scripts/Interactable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    public void GotTriggered()
+    {
+        anim.SetTrigger("Triggered");
+    }
+}
---
+++ SlimeOverRun/Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public Animator anim;
+    public GameObject connection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            connection.GetComponent<Interactable>().GotTriggered();
+            anim.SetTrigger("Triggered");
+            Debug.Log("Pisei");
+        }
+    }
+}
