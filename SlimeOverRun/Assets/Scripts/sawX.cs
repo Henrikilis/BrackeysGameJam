1c13464fcf03db6fb14cbc756275f344d375b256
Henrikilis <73853874+Henrikilis@users.noreply.github.com>
2021-02-18 16:41:41 -0300

    saws

---
+++ SlimeOverRun/Assets/Scripts/sawX.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class serraX : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public Transform pointA;
+    public Transform pointB;
+
+    public float sawSpeed;
+
+    public bool passedPointB;
+    public bool passedPointA;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        passedPointA = false;
+        passedPointB = false;
+    }
+
+
+    void FixedUpdate()
+    {
+        if (this.gameObject.transform.position.x < pointA.position.x)
+            rb.AddForce(new Vector3(sawSpeed, 0, 0), ForceMode.Acceleration);
+
+        else if (this.gameObject.transform.position.x > pointA.position.x)
+        {
+            passedPointA = true;
+        }
+
+        if (this.gameObject.transform.position.x > pointB.position.x && passedPointA)
+            rb.AddForce(new Vector3(-sawSpeed, 0, 0), ForceMode.Acceleration);
+        else if (this.gameObject.transform.position.x < pointB.position.x)
+        {
+            passedPointB = true;
+        }
+
+    }
+}
