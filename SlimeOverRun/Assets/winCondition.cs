3874fb504f6cffe3585d7b496b839ad6643f8849
Henrikilis <73853874+Henrikilis@users.noreply.github.com>
2021-02-18 15:26:40 -0300

    winCondition

---
+++ SlimeOverRun/Assets/winCondition.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class winCondition : MonoBehaviour
+{
+
+    public SlimeMovement[] sm;
+
+    [SerializeField]
+    private float slimeCount;
+
+    public  float slimeToWin;
+    private float slimeTotal;
+
+
+
+
+    void Start()
+    {
+        sm = FindObjectsOfType<SlimeMovement>();
+
+        for (int i = 0; i < sm.Length; i++)
+        {
+            slimeTotal++;
+        }
+
+        slimeToWin = slimeTotal / 3;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            slimeCount++;
+            if (slimeCount >= slimeToWin)
+            {
+                //passa de fase
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            slimeCount--;
+        }
+    }
+}
