ba2073aca42e9f6fd1efdddd0e288b60094577ab
Gabriel Lo Monaco Honório <75794887+Gabriel-LF@users.noreply.github.com>
2021-02-20 19:51:39 -0300

    Paladino

---
+++ SlimeOverRun/Assets/Scripts/Paladin.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Paladin : MonoBehaviour
+{
+    public int strengthNeeded;
+    public int currentStrength;
+    public bool attacking;
+    public bool dead;
+    public hpbar hp;
+    public slimeManager sm;
+    Camera cam;
+    public Animator anim;
+
+    public GameObject ballon;
+    private int displayText;
+    public TMP_Text text;
+
+    // Update is called once per frame
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        hp = FindObjectOfType<hpbar>();
+        sm = FindObjectOfType<slimeManager>();
+    }
+
+    private void Update()
+    {
+        displayText = strengthNeeded - currentStrength;
+
+        text.SetText(displayText.ToString());
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("MainSlime") && !dead)
+        {
+            anim.SetTrigger("Attack");
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") && attacking && !dead)
+        {
+            hp.decreaseHealth(1);
+            other.gameObject.SetActive(false);
+        }
+        if (other.CompareTag("MainSlime") && attacking && !dead)
+        {
+            hp.decreaseHealth(1);
+
+            cam = other.gameObject.GetComponentInChildren<Camera>();
+            cam.gameObject.transform.parent = null;
+            other.gameObject.SetActive(false);
+            sm.isDead();
+        }
+    }
+
+    public void StartAttack()
+    {
+        attacking = true;
+    }
+    public void EndAttack()
+    {
+        attacking = false;
+    }
+}
---
+++ SlimeOverRun/Assets/Scripts/PaladinDeath.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaladinDeath : MonoBehaviour
+{
+    public GameObject mainObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("MainSlime") && mainObject.GetComponent<Paladin>().dead == false)
+        {
+            mainObject.GetComponent<Paladin>().currentStrength++;
+            if (mainObject.GetComponent<Paladin>().currentStrength >= mainObject.GetComponent<Paladin>().strengthNeeded)
+            {
+                mainObject.GetComponent<Paladin>().ballon.SetActive(false);
+                mainObject.GetComponent<Paladin>().text.gameObject.SetActive(false);
+                mainObject.GetComponent<Paladin>().anim.SetTrigger("Death");
+                mainObject.gameObject.layer = 8;
+                mainObject.GetComponent<Paladin>().dead = true;
+            }
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("MainSlime") && mainObject.GetComponent<Paladin>().dead == false)
+        {
+            mainObject.GetComponent<Paladin>().currentStrength--;
+        }
+    }
+}
